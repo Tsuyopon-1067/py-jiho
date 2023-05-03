cb658bea72f43a8f345c77c72c28a152b8610ead
Tsuyopon-1067 <91947809+Tsuyopon-1067@users.noreply.github.com>
2023-05-03 14:05:58 +0900

    refactor: return type

--- main.py
+++ main.py
@@ -2,16 +2,16 @@ from datetime import datetime
 import time
 from TimeSchedule import TimeSchedule
 
-def main():
+def main() -> None:
 	ts = TimeSchedule()
 	clock()
 
-def clock():
+def clock() -> None:
 	while True:
 		tick()
 		time.sleep(1)
 
-def tick():
+def tick() -> None:
 	today_datetime_type = datetime.today()
 	now = int(today_datetime_type.strftime("%H%M"))
 	now_s = today_datetime_type.strftime("%H%:%M:%S")
